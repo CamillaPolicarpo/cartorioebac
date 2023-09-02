d2fe6fe389a5c831d9313ae1ec28f3bb4c04c2db
CamillaPolicarpo <143892650+CamillaPolicarpo@users.noreply.github.com>
2023-09-02 12:54:32 -0300

    Opção de saída
    Incluso no sistema opção de saída para o usuário.

--- cartoriopjebac.c
+++ cartoriopjebac.c
@@ -157,6 +157,7 @@ int main()
 	    printf("\t1- Registrar nomes\n");
     	printf("\t2- Consultar nomes\n");
     	printf("\t3- Deletar nomes\n\n");
+    	printf("\t4- Sair do sistema\n\n");
     	printf("Opção:"); //fim do menu
 	
         scanf("%d", &opcao);  //armazenando escolha do usuário
@@ -178,6 +179,10 @@ int main()
 			deletar();
    	    	break;
    	    	
+   	    	case 4:
+   	    	printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
    	    	
    	    	default:
 			printf("Essa opção não está disponível!\n");
